fc68860686fd1dc5b4fe4ccffda1e080e4d25f76
rajdhar78 <137742046+rajdhar78@users.noreply.github.com>
2025-09-12 12:18:41 +0530

    Update HelloController.java

--- src/main/java/com/example/spring_deploy_aws/HelloController.java
+++ src/main/java/com/example/spring_deploy_aws/HelloController.java
@@ -4,10 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RestMapping("/demo")    
 public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping("/data")
     public String home() {
         return "Hello, Spring Deploy AWS is working!";
+    }
+
+    @GetMapping("/data2")
+    public String home() {
+        return "Hello,Second update Spring Deploy AWS is working!";
     }
 }
