33ca58b27e7a83a92def67b2ada094df0192b926
rajdhar78 <137742046+rajdhar78@users.noreply.github.com>
2025-09-12 12:30:31 +0530

    Update HelloController.java

--- src/main/java/com/example/spring_deploy_aws/HelloController.java
+++ src/main/java/com/example/spring_deploy_aws/HelloController.java
@@ -1,12 +1,14 @@
 package com.example.spring_deploy_aws;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController   
+@RestController
+@RequestMapping("/")   // optional, can also just keep @GetMapping("/")
 public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping
     public String home() {
         return "Hello, Spring Deploy AWS is working!";
     }
